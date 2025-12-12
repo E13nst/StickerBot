ff6843c53c277697a0f67e83a4755e127cee6f1c
wind.slw <wind.slw@gmail.com>
2025-12-12 14:47:59 +0300

    feat: РґРѕР±Р°РІР»РµРЅР° РїРѕРґРґРµСЂР¶РєР° deep links РґР»СЏ РѕС‚РєСЂС‹С‚РёСЏ MiniApp РёР· С‚РµРєСЃС‚РѕРІС‹С… СЃСЃС‹Р»РѕРє

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,8 +1,12 @@
 # Utils package
 from .constants import STICKER_SIZE, STICKER_MAX_SIZE, WEBP_QUALITY
+from .links import create_miniapp_deeplink, decode_miniapp_start_param, create_miniapp_deeplink_simple
 
 __all__ = [
     'STICKER_SIZE',
     'STICKER_MAX_SIZE',
     'WEBP_QUALITY',
+    'create_miniapp_deeplink',
+    'decode_miniapp_start_param',
+    'create_miniapp_deeplink_simple',
 ]
